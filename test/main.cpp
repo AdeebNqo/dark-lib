14100929aa9fd16b82db9164da4b044a8bf190cb
AdeebNqo <adeebnqo@gmail.com>
2013-10-05 15:53:58 +0200

    cleaning up

---
+++ test/main.cpp
@@ -0,0 +1,8 @@
+#include<string>
+#include<iostream>
+#include<fstream>
+#include "idtag.h"
+int main(){
+	std::string mp3("/home/mhlzol004/Music/07 Bruno Mars - Natalie.mp3");
+	idtag tagger(mp3);
+}

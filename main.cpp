d38f7dc3c0694b3951810bd420c191aa1331cab5
AdeebNqo <adeebnqo@gmail.com>
2013-10-05 15:54:27 +0200

    deleted stuff

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include<string>
-#include<iostream>
-#include<fstream>
-#include "idtag.h"
-int main(){
-	std::string mp3("/home/mhlzol004/Music/07 Bruno Mars - Natalie.mp3");
-	idtag tagger(mp3);
-}
--- makefile
+++
@@ -1,8 +0,0 @@
-app: main.cpp idtag.o
-	g++ -o app main.cpp idtag.o
-idtag.o: idtag.cpp
-	g++ idtag.cpp -c
-clean:
-	rm -f *.o app
-run:
-	./app
